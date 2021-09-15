56fe6acb92fd55a6cd7e77183c8ee472215697bf
Rareș Cosma <rares@getbetter.ro>
2021-09-15 13:02:57 +0200

    update deps

--- setup.py
+++ setup.py
@@ -13,14 +13,13 @@ setup(
     packages=["rfh"],
     license="MIT",
     install_requires=[
-        "python-rofi==1.0.1",
         "sh",
         "i3ipc>=2.0.1",
         "click",
         "tuyapy==0.1.4",
         "lenses==1.1.0",
         "xdg",
-        "requests==2.26.0"
+        "requests>=2.26.0"
     ],
     entry_points="""
         [console_scripts]
