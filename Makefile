209e08f174f40208fa1014f37baf6e703a4e87dd
Rareș Cosma <rares@getbetter.ro>
2023-06-23 12:33:35 +0200

    feat: attempt build pipeline

--- Makefile
+++ Makefile
@@ -2,6 +2,9 @@ PROJECT=rfh
 ENTRYPOINT=$(PROJECT)/main.py
 PREFIX?=$(HOME)
 DESTDIR?=$(PREFIX)/bin
+BUILDPATH=dist/static
+BUILD_VERSION:=latest
+ARCH=linux-x86_64
 
 all: dist/$(PROJECT)
 
@@ -23,3 +26,18 @@ clean:
 .venv/freeze: .python-version
 	test -f .venv/bin/activate || python3 -mvenv .venv --prompt $(PROJECT)
 	. .venv/bin/activate && pip install -e . && pip freeze > .venv/freeze
+
+.PHONY: test
+test:
+	. .venv/bin/activate && pip install --no-cache-dir pytest && pytest
+
+build_static:
+	./portable/alpine-build.sh
+
+pack_static:
+	@rm -rf pack
+	@mkdir -p pack
+	@cd ${BUILDPATH} && tar -czvf ${PROJECT}-${BUILD_VERSION}-$(ARCH).tar.gz *
+	@mv ${BUILDPATH}/${PROJECT}-${BUILD_VERSION}-$(ARCH).tar.gz pack
+	@openssl sha256 < pack/${PROJECT}-${BUILD_VERSION}-$(ARCH).tar.gz | sed 's/^.* //' > pack/${PROJECT}-${BUILD_VERSION}-$(ARCH).sha256sum
+	@cat pack/${PROJECT}-${BUILD_VERSION}-$(ARCH).sha256sum
---
+++ portable/alpine-build.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env sh
+
+# This script builds a standalone statically linked musl binary
+# from the root project. Its main sequence is:
+#
+# - install build dependencies for staticx + whatever python
+#   requirements we desire to build locally (as opposed to wheel) -
+#   we'll refer to these as 'static' requirements
+# - install staticx to the root python
+# - create a virtual environment
+# - install non-static requirements via wheels
+# - install static requirements by building them locally
+# - produce a 'onefile' executable of the project using pyinstaller
+# - convert the executable to a statically linked one using staticx
+
+project="rfh"
+entrypoint="${project}/main.py"
+
+PIP_INSTALL="pip install --root-user-action=ignore --no-cache-dir"
+
+set -xe
+
+apk add --no-cache g++ musl-dev patchelf git make openssl
+$PIP_INSTALL -U wheel setuptools
+$PIP_INSTALL scons
+$PIP_INSTALL staticx
+
+my_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+oldpath="`pwd`"
+cd "$my_dir/.."
+
+rm -rf .venv
+python3 -m venv .venv
+set +x; source .venv/bin/activate; set -x
+
+$PIP_INSTALL .
+
+# produce a 'onefile' executable
+$PIP_INSTALL -I pyinstaller
+rm -rf dist build
+pyinstaller --onefile "${entrypoint}" -n "${project}"
+
+set +x; deactivate; set -x
+
+# make it portable using staticx
+mkdir -p dist/static
+staticx --loglevel INFO "dist/${project}" "dist/static/${project}"
+cd "$oldpath"
+
